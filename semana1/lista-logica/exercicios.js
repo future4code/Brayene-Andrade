0c58919fa91ae50b38d099b5a6d23a41574acde9
brayandrade <bray.andrade@gmail.com>
2021-06-18 19:33:43 -0300

    exercício-projeto-completo

--- semana1/lista-logica/exercicios.js
+++ semana1/lista-logica/exercicios.js
@@ -116,18 +116,36 @@ function checaIgualdadeDesconsiderandoCase(string1, string2) {
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
-  // implemente sua lógica aqui
+  const anoAtual = Number (prompt ("Digite o ano em que estamos"))
+  const anoNascimento = Number (prompt ("Digite o ano em que você nasceu"))
+  const anoEmissao = Number (prompt ("Digite o ano de emissão do seu documento"))
 
-}
+  const idade = anoAtual - anoNascimento
+  const tempoDeCarteira = anoAtual - anoEmissao
 
+  const cond1 = idade <=20 && tempoDeCarteira >=5
+  const cond2 = idade >20 && idade <= 50 && tempoDeCarteira >=10
+  const cond3 = idade > 50 && tempoDeCarteira >= 15 
+
+  console.log (cond1 || cond2 || cond3)
+
+} 
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
-  // implemente sua lógica aqui
+  const cond1 = ano % 400 === 0
+  const cond2 = (ano % 4 ===0) && (ano % 100 !== 0)
+
+  return cond1 || cond2
 
 }
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
-  // implemente sua lógica aqui
+
+  const  maiorDeIdade = prompt("Você tem mais de 18 anos?")
+  const  ensinoMedio = prompt("Você possui ensino médio completo?")
+  const  disponibilidade = prompt("Você possui disponibilidade exclusiva durante os horários do curso?")
+
+  console.log(maiorDeIdade === "sim" && ensinoMedio === "sim" && disponibilidade === "sim")
 
 }
