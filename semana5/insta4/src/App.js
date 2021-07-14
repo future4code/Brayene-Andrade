22b79897f37a4c5666583beabe1ef58ff8221cc6
brayandrade <bray.andrade@gmail.com>
2021-07-14 17:22:09 -0300

    novos usuarios

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -17,7 +17,20 @@ class App extends React.Component {
           nomeUsuario={'paulinha'}
           fotoUsuario={'https://picsum.photos/50/50'}
           fotoPost={'https://picsum.photos/200/150'}
+        />  
+        <Post
+          nomeUsuario={'thiaguinho'}
+          fotoUsuario={'https://stcotvfoco.com.br/2021/07/thiaguinho--1024x682.jpg'}
+          fotoPost={'https://picsum.photos/200/150?a=2'}
         />
+
+        <Post
+          nomeUsuario={'maisinha'}
+          fotoUsuario={'https://s2.glbimg.com/vSUhYzI-f591extb9ItknZBs95c=/620x520/e.glbimg.com/og/ed/f/original/2020/08/13/maisa-tiktoker.jpg'}
+          fotoPost={'https://picsum.photos/200/150?a=3'}
+        />
+
+
       </MainContainer>
     );
   }
