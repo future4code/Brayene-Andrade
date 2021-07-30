cb13a0bd75790adde0089e583cafd67debcc839b
brayandrade <bray.andrade@gmail.com>
2021-07-30 11:07:46 -0300

    nao sei o que alterou mas tenho que fazer um commit

--- semana7/spacex/integracao-spacex/src/App.js
+++ semana7/spacex/integracao-spacex/src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
 import axios from "axios";
+import styled from "styled components"
 
+ const stylePage = styled.div`
+  border: 1px solid black;
+  padding: 10px;
+  margin: 10px;
+  width: 300px;
+  display: flex;
+  justify-content: space-between;
+  `
 
 
 export default class App extends React.Component {
+
+ 
 
   state = {
     missions: []
