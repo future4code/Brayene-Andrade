1106c86c2867c6c0a874441df85bf4f94cb90251
brayandrade <bray.andrade@gmail.com>
2022-02-01 14:32:32 -0300

    exercicio1

--- semana7/projeto/labefy/src/App.js
+++ semana7/projeto/labefy/src/App.js
@@ -1,63 +1,62 @@
-import React from "react"
-import axios from "axios"
+import React from "react";
+import axios from "axios";
+import Header from "./components/Header/Header";
+import { AppContainer } from "./styled";
 
-const url = "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists"
+const url =
+  "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists";
 const headers = {
   headers: {
-    Authorization: 'bray-andrade-lovelace'
-  }
-}
+    Authorization: "bray-andrade-lovelace",
+  },
+};
 
 export default class App extends React.Component {
   state = {
     playlist: [],
     inputValue: "",
-    mostraPlaylist: false
-  }
+    mostraPlaylist: false,
+  };
 
   pegaPlaylist = (event) => {
-    this.setState({ inputValue: event.target.value })
-
-  }
+    this.setState({ inputValue: event.target.value });
+  };
 
   criarPlaylist = async () => {
     const body = {
-      name: this.state.inputValue
-    }
+      name: this.state.inputValue,
+    };
 
     await axios.post(url, body, headers);
-  }
+  };
 
   pegaListaPlaylist = async () => {
-    const res = await axios.get(url, headers)
-    this.setState({playlist: res.data.result.list, mostraPlaylist : true})
-  }
+    const res = await axios.get(url, headers);
+    this.setState({ playlist: res.data.result.list, mostraPlaylist: true });
+  };
 
-  deletaPlaylist = () => {
-
-  }
-
+  deletaPlaylist = () => {};
 
   render() {
-    const renderizaLista = this.state.playlist.map(({name , id}) => {
-     return <li key={id}>{name}</li>
+    const renderizaLista = this.state.playlist.map(({ name, id }) => {
+      return <li key={id}>{name}</li>;
+    });
 
-    })
-
-  
     return (
       <div>
-        <h1>Labefy</h1>
-        <input
-          placeholder={'Digite a sua playlist'}
-          value={this.state.inputValue}
-          onChange={this.pegaPlaylist}
-        />
-        <button onClick={this.criarPlaylist}> Criar Playlist </button>
-        <button onClick={this.pegaListaPlaylist}>Ver lista de Playlists</button>
-        <ul>
-          {this.state.mostraPlaylist && renderizaLista}
-        </ul>
+        <Header />
+        <AppContainer>
+          <input
+            placeholder={"Digite a sua playlist"}
+            value={this.state.inputValue}
+            onChange={this.pegaPlaylist}
+          />
+          <button onClick={this.criarPlaylist}> Criar Playlist </button>
+          <button onClick={this.pegaListaPlaylist}>
+            Ver lista de Playlists
+          </button>
+          <ul>{this.state.mostraPlaylist && renderizaLista}</ul>
+        </AppContainer>
       </div>
     );
   }
